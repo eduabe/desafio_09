36354378a75265c7731210d73e67a5fe9bdef004
Eduardo Abe <eduabe.n@gmail.com>
2021-02-24 23:28:57 -0300

    Finalizado cadastro de customers

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,20 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const createCustomerService = container.resolve(CreateCustomerService);
+    
+    const {name, email} = request.body;
+
+    const user = await createCustomerService.execute({
+      name,
+      email
+    });
+
+    if(!user){
+      return response.status(401).send();
+    }
+    
+    return response.status(201).json(user);
+    
   }
 }
