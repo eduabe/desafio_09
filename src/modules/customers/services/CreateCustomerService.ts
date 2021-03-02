2f74a33507d3b7bb1f8f22b19c120066cdac5fed
Eduardo Abe <eduabe.n@gmail.com>
2021-03-01 21:03:21 -0300

    Finalizado criação de usuários

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,25 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    
+    const userWithSameEmail = await this.customersRepository.findByEmail(email);
+
+    if(userWithSameEmail){
+      throw new AppError('Este email já está sendo usado.');
+    }
+
+    const user = await this.customersRepository.create({
+      name,
+      email
+    });
+
+    return user;
+
   }
 }
 
